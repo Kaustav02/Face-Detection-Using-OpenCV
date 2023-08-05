5a29ff15554f124e9af144c9fe485628cf135ee9
kaustav <kaustav.mondal2003@gmail.com>
2023-08-06 02:55:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import cv2
+face = cv2.CascadeClassifier("C:/Users/kaust/AppData/Local/Programs/Python/Python310/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+video_cap= cv2.VideoCapture(0)
+
+while True:
+    ret , video_data =video_cap.read()
+    color = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces = face.detectMultiScale(
+        color,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("live",video_data)
+    
+    if cv2.waitKey(10) == ord('e'):
+        break
+    
+video_cap.release()
